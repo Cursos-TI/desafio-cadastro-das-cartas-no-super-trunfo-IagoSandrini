ec8cba8c03487a7066c5d3dff0aa44a9a165147a
Iago Sandrini <iagosandrini@gmail.com>
2025-08-04 18:35:34 +0000

    Some ajusts

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main() {
     char state1;
     int code1;
-    char name1[20];
+    char name1[50];
     int population1;
     float area1;
     float pib1;
@@ -11,7 +11,7 @@ int main() {
 
     char state2;
     int code2;
-    char name2[20];
+    char name2[50];
     int population2;
     float area2;
     float pib2;
@@ -28,16 +28,16 @@ int main() {
     scanf(" %d", &code1);
 
     printf("Digite seu nome: \n");
-    scanf(" %s", name1);
+    scanf("%s", name1);
 
     printf("Digite o número de habitantes: \n");
     scanf(" %d", &population1);
 
     printf("Digite o tamanho da área: \n");
-    scanf(" %f", &area1);
+    scanf(" %2f", &area1);
 
     printf("Digite o valor do PIB: \n");
-    scanf(" %f", &pib1);
+    scanf(" %2f", &pib1);
 
     printf("Digite quantos pontos turísticos há: \n");
     scanf(" %d", &touristSpots1);
@@ -52,7 +52,7 @@ int main() {
     scanf(" %d", &code2);
 
     printf("Digite seu nome: \n");
-    scanf(" %s", name2);
+    scanf("%s", name2);
 
     printf("Digite o número de habitantes: \n");
     scanf(" %d", &population2);
@@ -72,7 +72,7 @@ int main() {
     printf("Código: %c0%d\n",state1, code1);
     printf("Nome: %s\n", name1);
     printf("População: %d\n", population1);
-    printf("Área: %f\n", area1);
+    printf("Área: %.2f\n", area1);
     printf("Número de Pontos Turísticos: %d\n", touristSpots1);
     
     printf("Carta 2\n");
@@ -80,7 +80,7 @@ int main() {
     printf("Código: %c0%d\n",state2, code2);
     printf("Nome: %s\n", name2);
     printf("População: %d\n", population2);
-    printf("Área: %f\n", area2);
+    printf("Área: %.2f\n", area2);
     printf("Número de Pontos Turísticos: %d\n", touristSpots2);
 
     return 0;
